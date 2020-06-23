d9e53c5eb23f785d880288552558982775370df4
Guangrui Wang <aguang.xyz@gmail.com>
2020-06-23 16:17:38 +1200

    Upgrade.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="auto-caption",
-    version="0.0.3",
+    version="0.0.4",
     author="Guangrui Wang",
     author_email="aguang.xyz@gmail.com",
     description="Automatic captioning.",
